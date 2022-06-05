96afc04e97a5251916d285b2beb55bac2239e505
Eslam Elgendey <101523625+Eslam-Elgendey@users.noreply.github.com>
2022-06-05 21:06:09 +0200

    Add files via upload

---
+++ task1.php
@@ -0,0 +1,22 @@
+<?php
+// task1
+$num1 = 5;
+$num2 = 10;
+$num1 = $num1 + $num2;
+$num2 = $num1 - $num2;
+$num1 = $num1 - $num2;
+
+
+echo $num1;
+echo $num2;
+
+?>
+
+
+
+
+
+
+
+
+
---
+++ task2.php
@@ -0,0 +1,5 @@
+
+<?php
+
+echo 'php course' , ' group 14';
+?>
